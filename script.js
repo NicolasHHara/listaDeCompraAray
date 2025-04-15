fe2604816ad76e5f953a1da54fa1b31258f9a41e
Nicolas <nico.hhara@gmail.com>
2025-04-15 08:40:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let produtos = [
+    {
+        foto: "./",
+        nome: "Ovo1",
+        preco: 20,
+    },
+    {
+        foto: "./",
+        nome:  "Ovo2",
+        preco: 30,
+    },
+    {
+        foto: "./",
+        nome:  "Ovo3",
+        preco: 40,
+    }
+]
+console.log(produtos[0].nome)
+
+    
