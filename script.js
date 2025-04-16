42c2ca945e3ba22bebad9568088a7cdfd5858b24
Nicolas <nico.hhara@gmail.com>
2025-04-15 21:37:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -44,16 +44,23 @@ function compra(){
 
 
 
-produtos.forEach(produto => {
-    const novaCaixa = document.createElement("div");
-    novaCaixa.classList.add("base");
+produtos.forEach(produto => {                           //pega todos os itens que esta deNtro do array "produto"
+    const novaCaixa = document.createElement("div");        //cria mais uma div chamada novaCaixa
+    novaCaixa.classList.add("base");                        //transforma a nova variavel "novaCaixa" em uma classe ja existente a base
 
-    novaCaixa.innerHTML = `
-        <img src="${produto.imagem}" alt="${produto.nome}">
-        <h1>${produto.nome}</h1>
-        <p>R$ ${produto.preco.toFixed(2)}</p>
-        <button class="btnComprar" onclick="compra('${produto.nome}')">Comprar</button>
+    novaCaixa.innerHTML = `                                     
+        <img src="${produto.imagem}" alt="">
+        <h1>${produto.nome}</h1>                                         
+        <p>R$ ${produto.preco.toFixed(2)}</p>                    
+        <button class="btnComprar" onclick="compra()">Comprar</button>
     `;
+                                            //no innerHTML ele cria a div "novaCaixa" no HTML
+                                            // to.Fixed(2) serve para que haja 2 casas após o ponto no preco
+                                            //para criar um codigo do html dentro do javascript e necessario colocar `` e em seguida colocar o que quer
+    
+    
 
-    container.appendChild(novaCaixa);
-});
+
+        container.appendChild(novaCaixa);  //coloca esse elemento dentro do container como filho
+                                            //para que eles nao fiquem em cima do primeiro produto (criado no HTMl)
+}); 
