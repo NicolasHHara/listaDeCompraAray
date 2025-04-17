0a8e7a8ff652625908709f35ba253404f5060136
Nicolas <nico.hhara@gmail.com>
2025-04-17 12:20:15 -0300

    inicioAside

--- script.js
+++ script.js
@@ -15,9 +15,8 @@ const produtos = [
         preco: 22.00,
         imagem: "",
     },
+    
 ];
-
-
 
 
 
@@ -38,7 +37,7 @@ produtos.forEach(produto => {                           //pega todos os itens qu
     
 
 
-        container.appendChild(novaCaixa);  //coloca esse elemento dentro do container como filho
+        document.getElementById('listagem').appendChild(novaCaixa);  //coloca esse elemento dentro do container como filho
                                             //para que eles nao fiquem em cima do primeiro produto (criado no HTMl)
 }); 
 
@@ -65,17 +64,16 @@ produtos.forEach(element => {
 */
 
 function compra(){
-    const novaCaixaAside = document.createElement("div");
-    novaCaixaAside.classList.add("baseAside");
+    const novaCaixaAside = document.createElement("aside");
+    novaCaixaAside.classList.add("containerAside");
 
     novaCaixaAside.innerHTML = `                                     
-        <img src="${produto.imagem}" alt="">
-        <h1>${produto.nome}</h1>                                         
-        <p>R$ ${produto.preco.toFixed(2)}</p>                    
-        <button class="btnComprar" onclick="compra()">Comprar</button>
-    `;
+        <div class"caixaAside">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket2-fill" viewBox="0 0 16 16">
+                <path d="M5.929 1.757a.5.5 0 1 0-.858-.514L2.217 6H.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h.623l1.844 6.456A.75.75 0 0 0 3.69 15h8.622a.75.75 0 0 0 .722-.544L14.877 8h.623a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1.717L10.93 1.243a.5.5 0 1 0-.858.514L12.617 6H3.383zM4 10a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm3 0a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm4-1a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1"/>
+            </svg>
+        </div>`;
                                             
-
 
         container.appendChild(novaCaixaAside);
                                           
