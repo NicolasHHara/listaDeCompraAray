918c2d0a25cf762acd83840e1290e363fff22cd2
Nicolas <nico.hhara@gmail.com>
2025-04-22 10:44:36 -0300

    faltaColocarIndice

--- script.js
+++ script.js
@@ -14,10 +14,26 @@ const produtos = [              //array
         preco: 22.00,
         imagem: "",
     },
-    
+
 ];
 
-
+const produtosAside = [
+    {
+        nome: "Ovo de Páscoa",
+        preco: 30.00,
+        imagem: "",
+    },
+    {
+        nome: "Chocolate Amargo",
+        preco: 15.50,
+        imagem: "",
+    },
+    {
+        nome: "Caixa de Bombons",
+        preco: 22.00,
+        imagem: "",
+    },
+];
 
 produtos.forEach(produto => {                           //pega todos os itens que esta deNtro do array "produto"
     const novaCaixa = document.createElement("div");        //cria mais uma div chamada novaCaixa
@@ -29,16 +45,16 @@ produtos.forEach(produto => {                           //pega todos os itens qu
         <p>R$ ${produto.preco.toFixed(2)}</p>                    
         <button class="btnComprar" onclick="compra()">Comprar</button>
     `;
-                                            //no innerHTML ele cria a div "novaCaixa" no HTML
-                                            // to.Fixed(2) serve para que haja 2 casas após o ponto no preco
-                                            //para criar um codigo do html dentro do javascript e necessario colocar `` e em seguida colocar o que quer
-    
-    
+    //no innerHTML ele cria a div "novaCaixa" no HTML
+    // to.Fixed(2) serve para que haja 2 casas após o ponto no preco
+    //para criar um codigo do html dentro do javascript e necessario colocar `` e em seguida colocar o que quer
 
 
-        document.getElementById('listagem').appendChild(novaCaixa);  //coloca esse elemento dentro do container como filho
-                                            //para que eles nao fiquem em cima do primeiro produto (criado no HTMl)
-}); 
+
+
+    document.getElementById('listagem').appendChild(novaCaixa);  //coloca esse elemento dentro do container como filho
+    //para que eles nao fiquem em cima do primeiro produto (criado no HTMl)
+});
 
 
 
@@ -81,31 +97,53 @@ produtos.forEach(element => {
 */
 
 
-    function compra() {
-        const novaCaixaAside = document.createElement("aside");
-        novaCaixaAside.classList.add("containerAside");
-        
-        if (document.querySelector(".containerAside")) {            //verifica se ja existe um aside
-            return; //se existir, nao repete
-        }
+function compra() {
+    const novaCaixaAside = document.createElement("aside");
+    novaCaixaAside.classList.add("containerAside");
 
-        novaCaixaAside.innerHTML = `                                     
+    if (document.querySelector(".containerAside")) {            //verifica se ja existe um aside
+        return; //se existir, nao repete
+    }
+
+    novaCaixaAside.innerHTML = `                                     
             <div class="caixaAside">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket2-fill" viewBox="0 0 16 16">
+                <svg id="imgAside" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket2-fill" viewBox="0 0 16 16">
                     <path d="M5.929 1.757a.5.5 0 1 0-.858-.514L2.217 6H.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h.623l1.844 6.456A.75.75 0 0 0 3.69 15h8.622a.75.75 0 0 0 .722-.544L14.877 8h.623a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1.717L10.93 1.243a.5.5 0 1 0-.858.514L12.617 6H3.383zM4 10a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm3 0a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm4-1a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1"/>
                 </svg>
                 <button id="btnExitAside"></button>
             </div>
         `;
-        document.getElementById('listagem').appendChild(novaCaixaAside);   //funciona da mesma forma anteriormente, para que o Aside se torne filho do contaienr
+    document.getElementById('listagem').appendChild(novaCaixaAside);   //funciona da mesma forma anteriormente, para que o Aside se torne filho do contaienr
+
+
+
+
+
+
+    produtos.forEach(produto => {                           //pega todos os itens que esta deNtro do array "produto"
+        const novaCaixaProduto = document.createElement("div");        //cria mais uma div chamada novaCaixa
+        novaCaixaProduto.classList.add("baseAside");                        //transforma a nova variavel "novaCaixa" em uma classe ja existente a base
+
+        novaCaixaProduto.innerHTML = `                                     
+            <img src="${produto.imagem}" alt="">
+            <h1>${produto.nome}</h1>                                         
+            <p>R$ ${produto.preco.toFixed(2)}</p>`;
+
+        
+        //no innerHTML ele cria a div "novaCaixa" no HTML
+        // to.Fixed(2) serve para que haja 2 cxasas após o ponto no preco
+        //para criar um codigo do html dentro do javascript e necessario colocar `` e em seguida colocar o que quer
+
+
+
+        document.querySelector(".containerAside").appendChild(novaCaixaProduto);
+        //document.getElementsByClassName("containerAside").appendChild(novaCaixaAside);  //coloca esse elemento dentro do container como filho
+
+    });
 }
 
-    function ("aside"){
-
-    }
 
 
-    
 /*let resposta = "sim";
 
 while (resposta === "sim") {
