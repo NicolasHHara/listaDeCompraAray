a47612ca9b70373693e6b7ba75a1fdb76af4724e
Nicolas <nico.hhara@gmail.com>
2025-04-24 12:21:39 -0300

    dsds

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const produtos = [              //array
+// array de produtos
+const produtos = [ 
     {
         nome: "Ovo de Páscoa",
         preco: 30.00,
@@ -14,20 +15,6 @@ const produtos = [              //array
         preco: 22.00,
         imagem: "",
     },
-
-];
-
-const produtosAside = [
-    {
-        nome: "Ovo de Páscoa",
-        preco: 30.00,
-        imagem: "",
-    },
-    {
-        nome: "Chocolate Amargo",
-        preco: 15.50,
-        imagem: "",
-    },
     {
         nome: "Caixa de Bombons",
         preco: 22.00,
@@ -35,26 +22,83 @@ const produtosAside = [
     },
 ];
 
-produtos.forEach(produto => {                           //pega todos os itens que esta deNtro do array "produto"
-    const novaCaixa = document.createElement("div");        //cria mais uma div chamada novaCaixa
-    novaCaixa.classList.add("base");                        //transforma a nova variavel "novaCaixa" em uma classe ja existente a base
+// array do carrinho de compras
+const produtosAside = new Array();
 
+
+// percorre todos os produtos do array e cria a listagem
+let contador = 0;
+produtos.forEach(produto => {
+
+    //cria mais uma div chamada novaCaixa
+    const novaCaixa = document.createElement("div");
+    //transforma a nova variavel "novaCaixa" em uma classe ja existente a base
+    novaCaixa.classList.add("base");
+    // cria o HTML da caixa
     novaCaixa.innerHTML = `                                     
         <img src="${produto.imagem}" alt="">
         <h1>${produto.nome}</h1>                                         
         <p>R$ ${produto.preco.toFixed(2)}</p>                    
-        <button class="btnComprar" onclick="compra()">Comprar</button>
-    `;
+        <button class="btnComprar" onclick="compra(${contador})">Comprar</button>`;
     //no innerHTML ele cria a div "novaCaixa" no HTML
     // to.Fixed(2) serve para que haja 2 casas após o ponto no preco
     //para criar um codigo do html dentro do javascript e necessario colocar `` e em seguida colocar o que quer
 
-
-
-
-    document.getElementById('listagem').appendChild(novaCaixa);  //coloca esse elemento dentro do container como filho
+    //coloca esse elemento dentro do container como filho
+    document.getElementById('listagem').appendChild(novaCaixa);  
     //para que eles nao fiquem em cima do primeiro produto (criado no HTMl)
+    contador++;
 });
+
+// metodo do clique do comprar
+function compra(item) {
+    // declara a classe
+    const novaCaixaAside = document.createElement("aside");
+    // adiciona o estilo para ele
+    novaCaixaAside.classList.add("containerAside");
+
+    //verifica se ja existe um aside
+    if (document.querySelector(".containerAside")) {
+        return; 
+    }
+    // cria a caixa
+    novaCaixaAside.innerHTML = `                                     
+            <div class="caixaAside">
+                <svg id="imgAside" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket2-fill" viewBox="0 0 16 16">
+                    <path d="M5.929 1.757a.5.5 0 1 0-.858-.514L2.217 6H.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h.623l1.844 6.456A.75.75 0 0 0 3.69 15h8.622a.75.75 0 0 0 .722-.544L14.877 8h.623a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1.717L10.93 1.243a.5.5 0 1 0-.858.514L12.617 6H3.383zM4 10a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm3 0a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm4-1a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1"/>
+                </svg>
+                <button id="btnExitAside"></button>
+            </div>`;
+
+    //funciona da mesma forma anteriormente, para que o Aside se torne filho do container
+    document.getElementById('listagem').appendChild(novaCaixaAside);
+
+    // adiciona o item do carrinho/produtoAside
+    produtosAside.push(produtos[item]);
+    // construir o HTML com base nos dados do array produtoAside
+
+    //forEach dos produtos aside criar os htmls dos produtos
+    produtosAside.forEach(produtosAside => {
+        //cria mais uma div chamada novaCaixa
+        const novaCaixaProdutoBase = document.createElement("div");
+        novaCaixaProdutoBase.classList.add("baseAside");
+
+        if (document.querySelector(".containerAside")) {
+            return; 
+        }
+        
+        novaCaixaProdutoBase.innerHTML = `                                     
+            <img src="${produtosAside.imagem}" alt="">
+            <h1>${produtosAside.nome}</h1>                                         
+            <p>R$ ${produtosAside.preco.toFixed(2)}</p>                    
+            <button class="btnremove" onclick="remove(${contador})">Remover</button>`
+
+        document.querySelector(".containerAside").appendChild(novaCaixaAside);
+    });
+    console.log(produtosAside);
+    
+
+}
 
 
 
@@ -96,9 +140,10 @@ produtos.forEach(element => {
         criava varios carrinhos um em cima do outro
 */
 
-
+/*
 function compra() {
     const novaCaixaAside = document.createElement("aside");
+
     novaCaixaAside.classList.add("containerAside");
 
     if (document.querySelector(".containerAside")) {            //verifica se ja existe um aside
@@ -115,12 +160,20 @@ function compra() {
         `;
     document.getElementById('listagem').appendChild(novaCaixaAside);   //funciona da mesma forma anteriormente, para que o Aside se torne filho do contaienr
 
+    //console.log(novaCaixaAside);
 
 
+    const novaCaixaProdutoBase = document.createElement("div");
+    novaCaixaProdutoBase.classList.add("baseAside");
 
+    novaCaixaProdutoBase.innerHTML = produtos.push(produtosAside);
+    
 
+    document.querySelector(".containerAside").appendChild(novaCaixaProdutoBase);
 
-    produtos.forEach((produto, index) => {                           //pega todos os itens que esta deNtro do array "produto"
+    console.log(novaCaixaProdutoBase);
+
+    /*produtos.forEach((produto, index) => {                           //pega todos os itens que esta deNtro do array "produto"
         const novaCaixaProduto = document.createElement("div");        //cria mais uma div chamada novaCaixa
         novaCaixaProduto.classList.add("baseAside");                        //transforma a nova variavel "novaCaixa" em uma classe ja existente a base
 
@@ -140,8 +193,10 @@ function compra() {
         document.querySelector(".containerAside").appendChild(novaCaixaProduto);
         //document.getElementsByClassName("containerAside").appendChild(novaCaixaAside);  //coloca esse elemento dentro do container como filho
 
-    });
-}
+    }); */
+//}
+
+
 
 
 
